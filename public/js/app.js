0fdbf9b1e6d0da0c854a3524d16ccdbbe1ba3f99
Larry Eisenstein <larryeisenstein@gmail.com>
2016-04-30 13:22:38 -0400

    Add input and submit to UI.

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,16 @@ socket.on('message', function(message) {
     console.log('New message: ');
     console.log(message.text);  
 });
+
+// Handles submitting a new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+    event.preventDefault(); // handle submission on our own, not browser
+    var $message = $('input[name=message]');
+    socket.emit('message', {
+        text: $message.val()
+    });
+    
+    $message.val('');
+});
