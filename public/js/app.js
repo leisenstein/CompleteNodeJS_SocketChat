033f3c2029f4f1427d622ae4056bb7cddbab872e
Larry Eisenstein <larryeisenstein@gmail.com>
2016-05-01 21:57:26 -0400

    Add timestamp and query params to display name and room

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,25 @@
+ // access to JS files loaded in index.html above app.js
+ var name = getQueryVariable('name') || 'Anonymous';
+ var room = getQueryVariable('room');
+
  //defined when you load socket.io library above
 var socket = io(); 
 
 socket.on('connect', function() {
     console.log('Connected to socket.io server!');
+    console.log(`${name} joined room: ${room}`);
+    //$('.messages').append(`<p>${name} joined room: ${room} </p>`);
 });
 
 socket.on('message', function(message) {
+    var momentTimestamp = moment.utc(message.timestamp);
+    var $message = $('.messages');
     console.log('New message: ');
     console.log(message.text);  
 
-    $('.messages').append(`<p>${message.text}</p>`);
+    $message.append(`<p><strong>` +  `${message.name}` + ` ` + `${momentTimestamp.local().format('h:mm a')}`   + `</strong></p>`);
+    $message.append(`<p>${message.text}</p>`);
+    //$('.messages').append(`<p><strong>(` + `${momentTimestamp.local().format('h:mm a')}` + `</strong>) ${message.text}</p>`);
 });
 
 // Handles submitting a new message
@@ -19,6 +29,7 @@ $form.on('submit', function (event) {
     event.preventDefault(); // handle submission on our own, not browser
     var $message = $('input[name=message]');
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 // create new server an use Express app as boilerplate
@@ -7,20 +8,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
     console.log('User connected via socket.io!');
 
     socket.on('message', function (message) {
+         message.timespamp = moment().valueOf();
          console.log('Message received: ' + message.text);
          // socket.broadcase.emit sends to all EXCEPT current connection
          io.emit('message', message); 
     });
+
     // custom event
     socket.emit('message', {
-        text: 'Welcome to the chat application!'
+        name: 'System',
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
     });
+
 });  // listen to events
 
 
